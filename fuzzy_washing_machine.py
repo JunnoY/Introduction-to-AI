d0d9edfb5dee4ce4217b4f38aff3c87b9dc12ddf
Junle <junle.yu@student.manchester.ac.uk>
2022-11-28 17:06:47 +0000

    lab1

--- fuzzy_washing_machine.py
+++ fuzzy_washing_machine.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import sys
 from typing import List
 from frules.rules import Rule as FuzzySet
 from frules.expressions import Expression as MemFunction
 from frules.expressions import ltrapezoid, trapezoid, rtrapezoid
-
 
 # membership functions and corresponding fuzzy sets for how dirty (in tablespoons)
 almost_clean_fn = MemFunction(ltrapezoid(0.25, 1.00), "almost_clean")
@@ -26,71 +25,103 @@ not_delicate_fn = MemFunction(rtrapezoid(6.00, 7.00), "not_delicate")
 not_delicate_set = FuzzySet(value=not_delicate_fn)
 
 # dictionary with the output level for each of the rules; the key pertains to the rule number
-rule_weights_dict = {1:10, 2:40, 3:60, 4:100}
+rule_weights_dict = {1: 10, 2: 40, 3: 60, 4: 100}
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # TASK 1: 2 marks
 # Implement the function that computes the degree to which a crisp input belongs to a fuzzy set
 def fuzzify(fuzzy_set: FuzzySet, val: float) -> float:
-	return 0
+    return fuzzy_set.eval(value=val)
 
 
 # TASK 2a: 2 marks
 # Implement the function for computing the conjunction of a rule's antecedents
 def get_conjunction(fuzzified_dirt: float, fuzzified_fabric_weight: float) -> float:
-	return 0
+    return min(fuzzified_dirt, fuzzified_fabric_weight)
+
 
 # TASK 2b: 2 marks
 # Implement the function for computing the disjunction of a rule's antecedents
 def get_disjunction(fuzzified_dirt: float, fuzzified_fabric_weight: float) -> float:
-	return 0
+    return max(fuzzified_dirt, fuzzified_fabric_weight)
 
 
 # TASK 3: 3 marks
 # Implement the function for computing the combined value of a rule antecedent
 def get_rule_antecedent_value(ant1: FuzzySet, val1: float, ant2: FuzzySet, val2: float, operator: str) -> float:
-	return 0
+    if ant1 is None:
+        degree_2 = fuzzify(ant2, val2)
+        return degree_2
+
+    if ant2 is None:
+        degree_1 = fuzzify(ant1, val1)
+        return degree_1
+
+    else:
+        degree_1 = fuzzify(ant1, val1)
+        degree_2 = fuzzify(ant2, val2)
+        if operator == "OR":
+            return get_disjunction(degree_1, degree_2)
+        elif operator == "AND":
+            return get_conjunction(degree_1, degree_2)
 
 
 # TASK 4: 2 marks
 # Implement function that returns the weighted output level of a rule
 def get_rule_output_value(rule_number: int, rule_antecedent_value: float) -> float:
-	return 0
+    return rule_weights_dict[rule_number] * rule_antecedent_value
+
+
+# return 0
 
 
 # TASK 5: 3 marks
 # dirt_amount can range from 0 to 2.5 inclusive
 # fabric_weight range from 1.0 to 11.00 inclusive
 def configure_washing_machine(dirt_amount: float, fabric_weight: float) -> tuple:
-	all_antecedents = [] # this should be set to a List containing the antecedent values for all rules
-	all_outputs = [] # this should be set to a List containing the output values for all rules
-	return (all_antecedents, all_outputs)
+    all_antecedents = []  # this should be set to a List containing the antecedent values for all rules
+    all_outputs = []  # this should be set to a List containing the output values for all rules
+    # a1 = get_rule_antecedent_value(None, 0.0, very_delicate_set, fabric_weight, "''")
+    # a2 = get_rule_antecedent_value(almost_clean_set, dirt_amount, delicate_set, fabric_weight, "OR")
+    # a3 = get_rule_antecedent_value(dirty_set, dirt_amount, delicate_set, fabric_weight, "AND")
+    # a4 = get_rule_antecedent_value(dirty_set, dirt_amount, not_delicate_set, fabric_weight, "AND")
+    all_antecedents.append(get_rule_antecedent_value(None, 0.0, very_delicate_set, fabric_weight, "''"))
+    all_antecedents.append(get_rule_antecedent_value(almost_clean_set, dirt_amount, delicate_set, fabric_weight, "OR"))
+    all_antecedents.append(get_rule_antecedent_value(dirty_set, dirt_amount, delicate_set, fabric_weight, "AND"))
+    all_antecedents.append(get_rule_antecedent_value(dirty_set, dirt_amount, not_delicate_set, fabric_weight, "AND"))
+    all_outputs.append(get_rule_output_value(1, all_antecedents[0]))
+    all_outputs.append(get_rule_output_value(2, all_antecedents[1]))
+    all_outputs.append(get_rule_output_value(3, all_antecedents[2]))
+    all_outputs.append(get_rule_output_value(4, all_antecedents[3]))
+    return (all_antecedents, all_outputs)
 
 
 # TASK 6: 3 marks
 # Implement function that computes the weighted average over all rules
 def get_weighted_average(all_antecedents: List, all_outputs: List) -> float:
-	return 0
+    return sum(all_outputs) / sum(all_antecedents)
 
 
 # TASK 7: 3 marks
 # Implement function for computing the actual temperature the machine should be set to
 def get_temperature(all_antecedents: List, all_outputs: List) -> float:
-	return 0
+    return 10 + get_weighted_average(all_antecedents, all_outputs) * 0.8
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Debug
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		cmd = "{}({})".format(sys.argv[1], ",".join(sys.argv[2:]))
-		print("debug run:", cmd)
-		ret = eval(cmd)
-		print("ret value:", ret)
-	else:
-		sys.stderr.write("Usage: fuzzy_washing_machine.py FUNCTION ARG...")
-		sys.exit(1)
+    if len(sys.argv) > 2:
+        cmd = "{}({})".format(sys.argv[1], ",".join(sys.argv[2:]))
+        print("debug run:", cmd)
+        ret = eval(cmd)
+        print("ret value:", ret)
+    else:
+        sys.stderr.write("Usage: fuzzy_washing_machine.py FUNCTION ARG...")
+        sys.exit(1)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # vim:set noet ts=4 sw=4:
